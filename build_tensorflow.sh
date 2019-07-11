16bf8517cc19a6f118520a1f404da5e5db43c340
Alan Zanoni Peixinho <alan.peixinho@tarsila.abtlus.org.br>
2019-07-10 23:04:42 -0300

    cuda 9

---
+++ build_tensorflow.sh
@@ -0,0 +1,18 @@
+CUDA_HOME="/usr/local/cuda-10.0"
+CUDNN_VERSION="7"
+NCCL_VERSION="2.2"
+
+ln -s $CUDA_HOME/lib64/stubs/libcuda.so $CUDA_HOME/lib64/stubs/libcuda.so.1
+export LD_LIBRARY_PATH="$CUDA_HOME/lib64:${LD_LIBRARY_PATH}:$CUDA_HOME/lib64/stubs"
+ldconfig
+
+wget https://github.com/tensorflow/tensorflow/archive/v1.12.2.zip
+unzip v1.12.2.zip
+cp script.exp ./tensorflow-1.12.2
+cd ./tensorflow-1.12.2
+./script.exp
+bazel build --jobs 20 -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+
+#create pip package
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
