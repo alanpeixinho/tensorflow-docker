9502ccf4d9d4db0f479428f90b33319c872d50c9
Peixinho <alan.peixinho@lnls.br>
2020-04-16 17:22:24 -0300

    tf 1.15

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ from nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04
 
 run apt-get update -y &&  apt-get install -y unzip libhdf5-dev libhdf5-serial-dev python3-h5py wget unzip zip python3 python3-dev python3-pip && rm -rf /var/lib/apt/lists
 run pip3 install --upgrade pip
-run pip3 install tensorflow-gpu==1.14.0  numpy==1.15.2 protobuf==3.6.1 six wheel mock Keras-Applications==1.0.8 Keras-Preprocessing==1.1.0
+run pip3 install tensorflow-gpu==1.15.2  numpy==1.15.2 protobuf==3.6.1 six wheel mock Keras-Applications==1.0.8 Keras-Preprocessing==1.1.0
 
--- build_tensorflow.sh
+++ build_tensorflow.sh
@@ -6,10 +6,10 @@ ln -s $CUDA_HOME/lib64/stubs/libcuda.so $CUDA_HOME/lib64/stubs/libcuda.so.1
 export LD_LIBRARY_PATH="$CUDA_HOME/lib64:${LD_LIBRARY_PATH}:$CUDA_HOME/lib64/stubs"
 ldconfig
 
-wget https://github.com/tensorflow/tensorflow/archive/v1.12.2.zip
-unzip v1.12.2.zip
-cp script.exp ./tensorflow-1.12.2
-cd ./tensorflow-1.12.2
+wget https://github.com/tensorflow/tensorflow/archive/v1.15.1.zip
+unzip v1.15.1.zip
+cp script.exp ./tensorflow-1.15.1
+cd ./tensorflow-1.15.1
 ./script.exp
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 
